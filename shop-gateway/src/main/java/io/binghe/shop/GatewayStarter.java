18657085b98390e63aca11afedb0704d0dabcb10
Jack <406151651@qq.com>
2022-08-11 17:30:48 +0800

    添加修改项目代码，项目整合SpringCloud Gateway网关相关代码。

---
+++ shop-gateway/src/main/java/io/binghe/shop/GatewayStarter.java
@@ -0,0 +1,19 @@
+package io.binghe.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author binghe (公众号：冰河技术)
+ * @version 1.0.0
+ * @description 服务网关启动类
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayStarter {
+
+    public static void main(String[] args){
+        SpringApplication.run(GatewayStarter.class, args);
+    }
+}
